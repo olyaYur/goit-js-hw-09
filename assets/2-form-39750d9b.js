68cb1a70006b254aba661869a46ccdac1e65ce73
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-20 19:45:26 +0000

    Deploying to gh-pages from @ olyaYur/goit-js-hw-09@c69af7385baa9c0ab5c1c8cef317e857fac00721 🚀

--- assets/2-form-39750d9b.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".feedback-form");document.querySelector("button");document.querySelector("input");const o="feedback-form-state";t.addEventListener("input",e=>{e.preventDefault();const l=e.currentTarget.elements.email.value,r=e.currentTarget.elements.message.value,a={email:l,message:r};console.log(a),localStorage.setItem(o,JSON.stringify(a))});t.addEventListener("submit",e=>{e.preventDefault(),console.log(localStorage.getItem(o)),localStorage.removeItem(o),t.reset()});
-//# sourceMappingURL=2-form-39750d9b.js.map
